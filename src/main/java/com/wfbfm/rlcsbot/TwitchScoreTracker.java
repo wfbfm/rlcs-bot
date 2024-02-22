b0cf4c7c0d76c9a9c3c5936ab8094ac7425c3fd7
wfbfm <wfbfmicou@gmail.com>
2024-02-22 00:19:33 +0000

    Remove unused imports

--- src/main/java/com/wfbfm/rlcsbot/TwitchScoreTracker.java
+++ src/main/java/com/wfbfm/rlcsbot/TwitchScoreTracker.java
@@ -1,11 +1,6 @@
 package com.wfbfm.rlcsbot;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -20,7 +15,6 @@ public class TwitchScoreTracker
     public static void main(String[] args)
     {
         // Set up the ChromeDriver
-        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--window-size=1920,1080");
@@ -99,22 +93,6 @@ public class TwitchScoreTracker
 
         // Close the browser when done
         driver.quit();
-    }
-
-    // Function to capture a frame from the video feed
-    private static byte[] captureFrame(WebElement videoElement)
-    {
-        TakesScreenshot screenshotDriver = (TakesScreenshot) videoElement;
-        byte[] screenshot = screenshotDriver.getScreenshotAs(OutputType.BYTES);
-
-        // Convert the screenshot to a Mat (OpenCV image)
-        Mat image = Imgcodecs.imdecode(new MatOfByte(screenshot), Imgcodecs.IMREAD_UNCHANGED);
-        Imgproc.cvtColor(image, image, Imgproc.COLOR_BGR2RGB);
-
-        // Convert the Mat back to bytes
-        MatOfByte matOfByte = new MatOfByte();
-        Imgcodecs.imencode(".png", image, matOfByte);
-        return matOfByte.toArray();
     }
 
     // Function to capture a screenshot of the entire webpage
