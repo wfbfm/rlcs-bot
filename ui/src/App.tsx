be78ecb4c080be56f9ff5c171a2ea32d9096c456
wfbfm <wfbfmicou@gmail.com>
2024-03-24 22:04:58 +0000

    Sort series events before displaying them

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -7,8 +7,8 @@ import { SeriesEventContainer } from './seriesEventContainer';
 const App: React.FC = () => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
   // FIXME: these can't be lists, they need to be maps - as the same ID can be sent down multiple times on update
-  const [seriesEvents, setSeriesEvents] = useState<(SeriesEvent)[]>([]);
-  const [series, setSeries] = useState<(Series)[]>([]);
+  const [seriesEvents, setSeriesEvents] = useState<{ [eventId: string]: SeriesEvent }>({});
+  const [series, setSeries] = useState<{ [seriesId: string]: Series }>({});
   const [messages, setMessages] = useState<(String)[]>([]);
 
   useEffect(() => {
@@ -21,23 +21,24 @@ const App: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    if (!socket)
-    {
+    if (!socket) {
       return;
-    } 
+    }
 
     socket.onmessage = (event) => {
-      console.log("event", event);
-      const newData = JSON.parse(event.data);
-      console.log("newData", newData);
-      if (newData._index === 'seriesevent')
-      {
-        console.log("it's a series event");
-        setSeriesEvents((prevSeriesEvents) => [...prevSeriesEvents, newData]);
-      } else if (newData._index === 'series')
-      {
-        console.log("it's a series");
-        setSeries((prevSeries) => [...prevSeries, newData]);
+      const socketData = JSON.parse(event.data);
+      if (socketData._index === 'seriesevent') {
+        setSeriesEvents((prevSeriesEvents) => {
+          const updatedSeriesEvents = { ...prevSeriesEvents };
+          updatedSeriesEvents[socketData._source.eventId] = socketData;
+          return updatedSeriesEvents;
+        });
+      } else if (socketData._index === 'series') {
+        setSeries((prevSeries) => {
+          const updatedSeries = { ...prevSeries };
+          updatedSeries[socketData._source.seriesId] = socketData;
+          return updatedSeries;
+        });
       }
       setMessages((prevMessages) => [...prevMessages, event.data]);
     }
@@ -49,29 +50,26 @@ const App: React.FC = () => {
 
   return (
     <div>
-      <h1>Hello, WebSocket!</h1>
-      <h2>All messages</h2>
-      <div>
-        {messages.map((message, index) => (
-          <div key={index}>
-            {message}
-          </div>
-        ))}
-      </div>
+      <h1>RLCS Commentary App</h1>
       <h2>All series events</h2>
       <div>
-        {seriesEvents.map((seriesEvent, index) => (
-          <div key={index}>
-            <SeriesEventContainer seriesEvent={seriesEvent} />
-          </div>
-        ))}
+        {Object.values(seriesEvents)
+          .sort((a, b) => {
+            const regex = /Event(\d+)-/;
+            const eventIdA = parseInt((a._source.eventId.match(regex) || [])[1], 10);
+            const eventIdB = parseInt((b._source.eventId.match(regex) || [])[1], 10);
+            return eventIdB - eventIdA;
+          })
+          .map((seriesEvent, index) => (
+            <div key={index}>
+              <SeriesEventContainer seriesEvent={seriesEvent} series={series[seriesEvent._source.seriesId]} />
+            </div>
+          ))}
       </div>
       <h2>All series</h2>
       <div>
-        {series.map((series, index) => (
-          <div key={index}>
-            {JSON.stringify(series)};
-          </div>
+        {Object.values(series).map((series, index) => (
+          <div key={index}>{JSON.stringify(series)}</div>
         ))}
       </div>
     </div>
--- ui/src/seriesEventContainer.tsx
+++ ui/src/seriesEventContainer.tsx
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import SeriesEvent from './model/seriesEvent';
-import { Box } from '@chakra-ui/react'; 
+import { Box } from '@chakra-ui/react';
+import Series from './model/series';
 
-export const SeriesEventContainer: React.FC<{ seriesEvent: SeriesEvent }> = ({ seriesEvent }) => {
-  return (
-    <Box>
-      {seriesEvent._source.commentary}
-    </Box>
-  );
+export const SeriesEventContainer: React.FC<{ seriesEvent: SeriesEvent , series: Series}> = ({ seriesEvent, series }) => {
+    return (
+        <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+            <Box bg='yellow.100' p={4}>
+                {seriesEvent._source.eventId}
+            </Box>
+            <Box bg='red.100' p={4}>
+                {series._source.blueTeam.teamName} - {series._source.orangeTeam.teamName}
+            </Box>
+            <Box bg='cyan.100' p={4}>
+                {seriesEvent._source.seriesScore.blueScore} - {seriesEvent._source.seriesScore.orangeScore}
+            </Box>
+            <Box bg='green.100' p={4}>
+                {seriesEvent._source.currentGame.score.blueScore} - {seriesEvent._source.currentGame.score.orangeScore}
+            </Box>
+            <Box bg='black.100' p={4}>
+                {seriesEvent._source.commentary}
+            </Box>
+        </Box>
+    );
 };
