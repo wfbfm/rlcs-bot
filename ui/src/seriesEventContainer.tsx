63b22400836808e3881c5f7f329bd988763a3f79
wfbfm <wfbfmicou@gmail.com>
2024-03-25 23:09:08 +0000

    Add goal/game icons and highlighting

--- ui/src/seriesEventContainer.tsx
+++ ui/src/seriesEventContainer.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import SeriesEvent from './model/seriesEvent';
-import { Box, Center, Divider, HStack, Stack, Text, VStack } from '@chakra-ui/react';
+import { Badge, Box, Center, Divider, HStack, Icon, Stack, Text, VStack } from '@chakra-ui/react';
 import Series from './model/series';
 import { MinusIcon, TimeIcon } from '@chakra-ui/icons';
+import { MdOutlineSportsScore } from "react-icons/md";
+import { IoFootball } from "react-icons/io5";
 
 
 const seriesScoreIcons = (seriesWinningGameScore: number, seriesScore: number, colour: string, filledFirst: boolean) => {
@@ -22,17 +24,56 @@ const seriesScoreIcons = (seriesWinningGameScore: number, seriesScore: number, c
     return icons;
 };
 
+// TODO: this is a placeholder to test out some formatting in the UI.  This kind of logic will live in the backend.
+function getEvaluationResult(seriesEvent: SeriesEvent): string {
+    const { evaluation, currentGame, seriesScore } = seriesEvent._source;
+
+    if (evaluation === 'GAME_SCORE_CHANGED') {
+        if (currentGame.score.blueScore > currentGame.score.orangeScore) {
+            return 'BLUE_GOAL';
+        } else {
+            return 'ORANGE_GOAL';
+        }
+    } else if (evaluation === 'SERIES_SCORE_CHANGED') {
+        if (seriesScore.blueScore > seriesScore.orangeScore) {
+            return 'BLUE_GAME';
+        } else {
+            return 'ORANGE_GAME';
+        }
+    } else {
+        return ''; // Handle other evaluation values if necessary
+    }
+}
+
+function getBackgroundColour(evaluationResult: string): string {
+    if (evaluationResult === 'BLUE_GOAL') {
+        return 'blue.100';
+    }
+    if (evaluationResult === 'BLUE_GAME') {
+        return 'blue.200';
+    }
+    if (evaluationResult === 'ORANGE_GOAL') {
+        return 'orange.100';
+    }
+    if (evaluationResult === 'ORANGE_GAME') {
+        return 'orange.200';
+    }
+    return 'gray.200';
+}
+
 export const SeriesEventContainer: React.FC<{ seriesEvent: SeriesEvent, series: Series }> = ({ seriesEvent, series }) => {
+    const evaluationResult = getEvaluationResult(seriesEvent);
+    const backgroundColour = getBackgroundColour(evaluationResult);
+
     return (
         <Box maxW='500px' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-            <Box bg='blue.100'>
-                <Text as='i' fontSize='sm'>Game {seriesEvent._source.currentGameNumber}</Text>
-            </Box>
             <Box>
-                <Box bg='gray.200' p={2}>
+                <Center bg={backgroundColour} p={2}>
                     <HStack>
-                        <Box minW='10px'>
-                        </Box>
+                        <Center minW='20px'>
+                        {evaluationResult === 'BLUE_GOAL' && <Icon boxSize={6} as={IoFootball} color={'blue.500'}></Icon>}
+                        {evaluationResult === 'BLUE_GAME' && <Icon boxSize={6} as={MdOutlineSportsScore} color={'blue.500'}></Icon>}
+                        </Center>
                         <VStack>
                             <Text as='b' fontSize='sm'>{series._source.blueTeam.teamName}</Text>
                             <HStack>
@@ -46,29 +87,32 @@ export const SeriesEventContainer: React.FC<{ seriesEvent: SeriesEvent, series: 
                                         {seriesEvent._source.currentGame.score.blueScore}
                                     </Text>
                                 </Box>
-                                    <Text as='b' fontSize='m'>
-                                        -
-                                    </Text>
-                                <Box p={1.5} bg='orange.300' borderRadius='lg'>
+                                <VStack p={2} spacing={0}>
+                                    <TimeIcon boxSize={3.5} m={0}></TimeIcon>
+                                    <Text as='b' fontSize='sm' m={0}>{seriesEvent._source.currentGame.clock.displayedTime}</Text>
+                                </VStack>
+                                <Box p={1.5} bg='orange.400' borderRadius='lg'>
                                     <Text as='b' fontSize='m' color={'white'}>
                                         {seriesEvent._source.currentGame.score.orangeScore}
                                     </Text>
                                 </Box>
                             </HStack>
                             <HStack>
-                                <TimeIcon></TimeIcon>
-                                <Text as='b' fontSize='sm'>{seriesEvent._source.currentGame.clock.displayedTime}</Text>
                             </HStack>
                         </VStack>
                         <VStack>
                             <Text as='b' fontSize='sm'>{series._source.orangeTeam.teamName}</Text>
                             <HStack>
-                                {seriesScoreIcons(series._source.seriesWinningGameScore, seriesEvent._source.seriesScore.orangeScore, 'orange.300', true)}
+                                {seriesScoreIcons(series._source.seriesWinningGameScore, seriesEvent._source.seriesScore.orangeScore, 'orange.400', true)}
                             </HStack>
                         </VStack>
+                        <Center minW='20px'>
+                        {evaluationResult === 'ORANGE_GOAL' && <Icon boxSize={6} as={IoFootball} color={'orange.400'}></Icon>}
+                        {evaluationResult === 'ORANGE_GAME' && <Icon boxSize={6} as={MdOutlineSportsScore} color={'orange.400'}></Icon>}
+                        </Center>
                     </HStack>
-                </Box>
-                <Box bg='black.100' p={4}>
+                </Center>
+                <Box bg='gray.100' p={4}>
                     <Text fontSize='sm'>
                         {seriesEvent._source.commentary}
                     </Text>
