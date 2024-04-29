83f0ba7f6c6158a0a5b8ad00559abb72843ed9c4
wfbfm <wfbfmicou@gmail.com>
2024-04-29 22:32:39 +0100

    Improve sidebar logos

--- ui/src/seriesContainer.tsx
+++ ui/src/seriesContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Badge, Box, Center, Divider, HStack, Image, Icon, Stack, Text, VStack, useColorMode, Grid, GridItem, Flex, Spacer } from '@chakra-ui/react';
+import { Badge, Box, Center, Divider, HStack, Image, Icon, Stack, Text, VStack, useColorMode, Grid, GridItem, Flex, Spacer, useColorModeValue } from '@chakra-ui/react';
 import Series from './model/series';
 
 const getLogoName = (teamName: string | undefined, isLightMode: boolean): string | undefined => {
@@ -13,55 +13,68 @@ const getLogoName = (teamName: string | undefined, isLightMode: boolean): string
 
 export const SeriesContainer: React.FC<{ series: Series, logos: { [logoName: string]: string } }> = ({ series, logos }) => {
 
-    const blueColour: string = series._source.complete ? (series._source.seriesScore.teamInLead === 'BLUE' ? 'blue.500' : 'gray.400') : 'teal.300'
-    const orangeColour: string = series._source.complete ? (series._source.seriesScore.teamInLead === 'ORANGE' ? 'orange.400' : 'gray.400') : 'teal.300'
     const { colorMode, toggleColorMode } = useColorMode();
     const isLightMode = colorMode === 'light';
+    const defaultColour: string = useColorModeValue('gray.400', 'gray.700');
+    const blueColour: string = series._source.complete ? (series._source.seriesScore.teamInLead === 'BLUE' ? 'blue.500' : defaultColour) : defaultColour;
+    const orangeColour: string = series._source.complete ? (series._source.seriesScore.teamInLead === 'ORANGE' ? 'orange.400' : defaultColour) : defaultColour;
     const blueLogoName = getLogoName(series?._source.blueTeam.teamName, isLightMode);
     const orangeLogoName = getLogoName(series?._source.orangeTeam.teamName, isLightMode);
     const blueLogo = blueLogoName ? logos[blueLogoName] : undefined;
     const orangeLogo = orangeLogoName ? logos[orangeLogoName] : undefined;
 
     return (
-            <>
-        <Box width='100%' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-            <HStack p={2} width='100%' justify="space-between">
-                <Flex width='40%'>
-                    <Center paddingRight={2}>
-                        {blueLogo && (
-                            <Image src={`${blueLogo}`} boxSize={6} mr={2}></Image>
-                        )}
-                        <Text as='b' fontSize='xs' mr={2}>
-                            {series._source.blueTeam.teamName}
-                        </Text>
-                    </Center>
-                </Flex>
+        <>
+            <Box width='100%' overflow='hidden' p={2} borderRadius={'lg'} border={'1px solid gray'}>
+                <Flex>
+                    <Flex width='40%'>
+                        <Center>
+                            {blueLogo && (
+                                <Image src={`${blueLogo}`} boxSize={6} mr={2}></Image>
+                            )}
+                        </Center>
+                        <Spacer></Spacer>
+                        <Center paddingRight={2}>
+                            <Center>
+                                <Text as='b' fontSize='xs' mr={2} alignItems='center'>
+                                    {series._source.blueTeam.teamName}
+                                </Text>
+                            </Center>
+                        </Center>
+                    </Flex>
 
-                <HStack width='20%' justify="center">
-                    <Box p={1.5} bg={blueColour} borderRadius='lg' mr={2}>
-                        <Text as='b' fontSize='m'>
-                            {series._source.seriesScore.blueScore}
-                        </Text>
-                    </Box>
-                    <Box p={1.5} bg={orangeColour} borderRadius='lg' mr={2}>
-                        <Text as='b' fontSize='m'>
-                            {series._source.seriesScore.orangeScore}
-                        </Text>
-                    </Box>
-                </HStack>
+                    <Spacer />
 
-                <Flex width='40%'>
-                    <Center>
-                        <Text as='b' fontSize='xs' mr={2}>
-                            {series._source.orangeTeam.teamName}
-                        </Text>
-                        {orangeLogo && (
-                            <Image src={`${orangeLogo}`} boxSize={6} mr={2}></Image>
-                        )}
-                    </Center>
+                    <HStack width='20%' justify="center">
+                        <Box p={1.5} bg={blueColour} borderRadius='lg' mr={2}>
+                            <Text as='b' fontSize='m'>
+                                {series._source.seriesScore.blueScore}
+                            </Text>
+                        </Box>
+                        <Box p={1.5} bg={orangeColour} borderRadius='lg' mr={2}>
+                            <Text as='b' fontSize='m'>
+                                {series._source.seriesScore.orangeScore}
+                            </Text>
+                        </Box>
+                    </HStack>
+
+                    <Spacer />
+
+                    <Flex width='40%'>
+                        <Center paddingLeft={2}>
+                            <Text as='b' fontSize='xs' mr={2}>
+                                {series._source.orangeTeam.teamName}
+                            </Text>
+                        </Center>
+                        <Spacer>                        </Spacer>
+                        <Center>
+                            {orangeLogo && (
+                                <Image src={`${orangeLogo}`} boxSize={6} mr={2}></Image>
+                            )}
+                        </Center>
+                    </Flex>
                 </Flex>
-            </HStack>
-        </Box>
-    </>
+            </Box>
+        </>
     );
 };
